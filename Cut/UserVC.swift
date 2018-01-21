1ae1d405fd30efb6330909e4a428f7d2f840743d
Kyle McAlpine <k@kylejm.io>
2018-01-21 20:01:59 +0000

    WIP creating UserVC

---
+++ Cut/UserVC.swift
@@ -0,0 +1,17 @@
+//
+//  UserVC.swift
+//  Cut
+//
+//  Created by Kyle McAlpine on 21/01/2018.
+//  Copyright © 2018 Kyle McAlpine. All rights reserved.
+//
+
+import UIKit
+
+class UserVC: UIViewController {
+    let user: User
+    
+    init(user: User) {
+        
+    }
+}
---
+++ Cut/UserView.swift
@@ -0,0 +1,25 @@
+//
+//  UserView.swift
+//  Cut
+//
+//  Created by Kyle McAlpine on 21/01/2018.
+//  Copyright © 2018 Kyle McAlpine. All rights reserved.
+//
+
+import UIKit
+
+class UserView: UIView {
+    let usernameLabel = UILabel()
+    let followerCountLabel = UILabel()
+    let followingCountLabel = UILabel()
+    let followButton = UIButton()
+    let segmentedControl = UISegmentedControl(items: ["Watch List", "Ratings"])
+    
+    init(user: User) {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
